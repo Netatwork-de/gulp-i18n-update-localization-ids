50f687eadf800e7f4c5be6f702926aad06416301
Max J. Polster <Max.Polster@netatwork.de>
2019-01-23 15:08:23 +0100

    Change ignore map behaviour.

--- test/ignore-map.js
+++ test/ignore-map.js
@@ -5,9 +5,9 @@ const IgnoreMap = require('../lib/ignore-map');
 
 test('ignore content (regexp)', t => {
     const ignore = new IgnoreMap().use({
-        content: /foo/
+        content: /^foo/
     });
-    t.true(ignore.content('foo bar'));
+    t.true(ignore.content(' foo bar '));
     t.false(ignore.content('bar baz'));
 });
 
@@ -15,7 +15,7 @@ test('ignore content (fixed)', t => {
     const ignore = new IgnoreMap().use({
         content: 'foo bar'
     });
-    t.true(ignore.content('foo bar'));
+    t.true(ignore.content(' foo bar '));
     t.false(ignore.content('foo bar baz'));
 });
 
@@ -23,17 +23,19 @@ test('ignore content (function)', t => {
     const ignore = new IgnoreMap().use({
         content: v => v === '42'
     });
-    t.true(ignore.content('42'));
+    t.true(ignore.content(' 42 '));
     t.false(ignore.content('foo'));
 });
 
 test('option normalization & validation', t => {
-    const ignore = new IgnoreMap().use({
-        use: [
-            {content: 'foo'}
+    const ignore = new IgnoreMap().use([
+        [
+            {content: 'foo'},
+            {},
+            []
         ]
-    });
-    t.true(ignore.content('foo'));
+    ]);
+    t.true(ignore.content(' foo '));
     t.false(ignore.content('bar'));
 
     t.throws(() => ignore.use(42));
