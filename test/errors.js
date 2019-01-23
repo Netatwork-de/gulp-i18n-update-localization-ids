40f34864784d3d1ba5e652b30f5f8c9e4a29ed5f
Max J. Polster <Max.Polster@netatwork.de>
2019-01-23 11:12:43 +0100

    test non whitelisted content is localized.

--- test/errors.js
+++ test/errors.js
@@ -21,6 +21,12 @@ test('non-whitelisted attribute is already localized', transformFailsTest({
     <h1 t="[foo]baz" foo="bar"></h1>
 `, /attribute.*localized/i));
 
+test('non-whitelisted content is already localized', transformFailsTest({
+    whitelist: [{tagName: 'h1', content: false}]
+}, `
+    <h1 t="[text]foo">Hello World</h1>
+`, /content.*localized/i));
+
 test('mixed content', transformFailsTest({
     whitelist: [{tagName: 'h1'}]
 }, `
