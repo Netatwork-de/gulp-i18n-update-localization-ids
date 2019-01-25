d07ba69f71c719a2b96177b9241bf372a127b653
Max J. Polster <Max.Polster@netatwork.de>
2019-01-25 09:58:45 +0100

    add merge options utility.

---
+++ test/merge-options.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const test = require('ava');
+const mergeOptions = require('../lib/merge-options');
+
+test('whitelist', t => {
+    t.deepEqual(
+        mergeOptions({}, {}),
+        {whitelist: []}
+    );
+    t.deepEqual(
+        mergeOptions({whitelist: ['foo']}, {}),
+        {whitelist: ['foo']}
+    );
+    t.deepEqual(
+        mergeOptions({whitelist: ['foo']}, {whitelist: ['bar', 'baz']}),
+        {whitelist: ['foo', 'bar', 'baz']}
+    );
+});
+
+test('ignore', t => {
+    t.is(mergeOptions({}, {}).ignore, undefined);
+    t.is(mergeOptions({ignore: 'foo'}, {}).ignore, 'foo');
+    t.is(mergeOptions({}, {ignore: 'bar'}).ignore, 'bar');
+    t.deepEqual(
+        mergeOptions({ignore: 'foo'}, {ignore: 'bar'}).ignore,
+        ['foo', 'bar']
+    );
+});
+
+test('atomic options', t => {
+    for (const key of ['emit', 'idTemplate', 'keyAttribute', 'encoding', 'LocalizationKey']) {
+        t.is(mergeOptions({[key]: 'foo'}, {})[key], 'foo');
+        t.is(mergeOptions({}, {[key]: 'foo'})[key], 'foo');
+        t.is(mergeOptions({[key]: 'foo'}, {[key]: 'bar'})[key], 'bar');
+    }
+});
